642ec83449b4063de363eab5872dae9515a90cc3
rakib8680 <rakib.cst.1st@gmail.com>
2023-06-16 14:04:17 +0600

    added to public git repo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
@@ -141,12 +142,37 @@ async function run() {
       res.send(result)
     });
 
-     // get single class 
-     app.get('/class/:id', async (req, res) => {
+    // get single class 
+    app.get('/class/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await classCollection.findOne(query);
       res.send(result)
+    });
+
+
+
+    // get single class 
+    app.get('/payment/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.findOne(query);
+      res.send(result)
+    });
+
+    // create payment intent 
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+
     })
 
 
